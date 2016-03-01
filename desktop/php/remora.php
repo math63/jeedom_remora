84a26d8cd7d3aa429e58ee585fe42d8a6c055b37
Cedric Dupont <cedric@lunaria.wayang.fr>
2016-03-01 22:54:26 +0100

    Correction log

--- desktop/php/remora.php
+++ desktop/php/remora.php
@@ -51,7 +51,7 @@ $eqLogics = eqLogic::byType('remora');
         echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
         echo "<center>";
         $test = 'node_' . $eqLogic->getConfiguration('icone') . '.png';
-        log::add('playbulb', 'debug', 'test ' . $test);
+        log::add('remora', 'debug', 'test ' . $test);
         if (in_array($test, $files)) {
           $path = 'node_' . $eqLogic->getConfiguration('icone');
         } else {
