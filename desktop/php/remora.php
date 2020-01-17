2be9cfe9e877dd11fe494c5339581520b81ebf2c
DUPONT <cedric.dupont@atos.net>
2020-01-17 12:23:11 +0100

    new look

--- desktop/php/remora.php
+++ desktop/php/remora.php
@@ -9,10 +9,9 @@ $eqLogics = eqLogic::byType('remora');
 ?>
 
 <div class="row row-overflow">
-  <div class="col-lg-2 col-sm-3 col-sm-4">
+  <div class="col-lg-2 col-sm-3 col-sm-4" id="hidCol" style="display: none;">
     <div class="bs-sidebar">
       <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-        <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fas fa-plus-circle"></i> {{Ajouter un équipement}}</a>
         <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
         <?php
         foreach ($eqLogics as $eqLogic) {
@@ -23,41 +22,33 @@ $eqLogics = eqLogic::byType('remora');
     </div>
   </div>
 
-  <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
- <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
- <div class="eqLogicThumbnailContainer">
-   <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-   <center>
-     <i class="fas fa-wrench" style="font-size : 5em;color:#767676;"></i>
-   </center>
-   <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
- </div>
-</div>
+  <div class="col-lg-12 eqLogicThumbnailDisplay" id="listCol">
+    <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
+    <div class="eqLogicThumbnailContainer logoPrimary">
 
-    <legend><i class="fas fa-table"></i>  {{Mes remora}}
-    </legend>
-    <div class="eqLogicThumbnailContainer">
-      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-        <center>
-          <i class="fas fa-plus-circle" style="font-size : 7em;color:#00979c;"></i>
-        </center>
-        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
+      <div class="cursor eqLogicAction logoSecondary" data-action="add">
+          <i class="fas fa-plus-circle"></i>
+          <br/>
+        <span>{{Ajouter}}</span>
       </div>
+      <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+        <i class="fas fa-wrench"></i>
+        <br/>
+        <span>{{Configuration}}</span>
+      </div>
+
+    </div>
+
+    <input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+
+    <legend><i class="fas fa-home" id="butCol"></i> {{Mes Equipements}}</legend>
+    <div class="eqLogicThumbnailContainer">
       <?php
-      $dir = dirname(__FILE__) . '/../../doc/images/';
-      $files = scandir($dir);
       foreach ($eqLogics as $eqLogic) {
         $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
         echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
         echo "<center>";
-        $test = 'node_' . $eqLogic->getConfiguration('icone') . '.png';
-        log::add('remora', 'debug', 'test ' . $test);
-        if (in_array($test, $files)) {
-          $path = 'node_' . $eqLogic->getConfiguration('icone');
-        } else {
-          $path = 'remora_icon';
-        }
-        echo '<img src="plugins/remora/doc/images/' . $path . '.png" height="105" width="95" />';
+        echo '<img src="plugins/remora/plugin_info/remora_icon.png" height="105" width="95" />';
         echo "</center>";
         echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
         echo '</div>';
